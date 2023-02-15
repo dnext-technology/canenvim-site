46c4ac45d945ca50bbe25b89e8b6302a49b6345f
habib <habib.mevlut@pia-team.com>
2023-02-15 15:10:01 +0300

    unused import deleted

--- src/pages/guest/viev/guestPageView.js
+++ src/pages/guest/viev/guestPageView.js
@@ -10,7 +10,7 @@ import '../style/guestPageStyles.scss';
 import DataTable from 'react-data-table-component';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faRemove } from '@fortawesome/free-solid-svg-icons'
-import moment from "moment";
+import moment from 'moment';
 
 const GuestPage = () => {
   const {REACT_APP_BASE_URL, REACT_APP_BOOKING_API, REACT_APP_LOCATION_API} = process.env;
